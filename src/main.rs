cf379a443e44aebc20964e8931c6a781ade347e6
FairyScript <h@queb.fun>
2021-11-29 04:16:27 +0800

    libtest

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use serde::Deserialize;
+use std::collections::HashMap;
+
+#[derive(Deserialize, Debug)]
+struct Item {
+    foo: String,
+    bar: String,
+}
+
+type Entry = HashMap<String, Vec<Item>>;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("https://httpbin.org/ip")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    println!("{:#?}", resp);
+    println!("{:#?}", resp["origin"]);
+
+    let toml_string = r#"
+  [[entry]]
+  foo = "a0"
+  bar = "b0"
+
+  [[entry]]
+  foo = "a1"
+  bar = "b1"
+  "#;
+    let config: Entry = toml::from_str(toml_string).unwrap();
+    println!("{:#?}", config);
+
+    Ok(())
 }
